20b3480d64780f9a2d73ba90b64137dd4eb7dac8
Satish-1922 <satish.k@lfcpltd.com>
2026-01-20 15:24:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const http = require('http');
 
+const express = require('express'); // Import Express.js 
+const axios = require('axios'); // Import axios
+const app = express(); // Create an Express app
+app.use(express.json()); // Middleware to parse JSON bodies
+
+// Set port and verify_token
+const port = process.env.PORT || 3000;
+const verifyToken = process.env.VERIFY_TOKEN;
+
 const options = {
   hostname: '115.124.124.66',
   port: 80,
